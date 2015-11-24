da9c27eea46b6bdb3e5d2488753f4368d74dcefa
Denis Griffis <denisrgriffis@gmail.com>
2015-11-24 13:58:10 -0500

    Added scaling options to memory funcs

--- py/denis/common/memory.py
+++ py/denis/common/memory.py
@@ -5,8 +5,16 @@ Code source: http://code.activestate.com/recipes/286222/
 import os
 _proc_status = '/proc/%d/status' % os.getpid()
 
-_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
-          'KB': 1024.0, 'MB': 1024.0*1024.0}
+_B=1.0
+_KB=1024.0
+_MB=1024.0**2
+_GB=1024.0**3
+_scale = {
+    'B': _B, 'b': _B,
+    'kB': _KB, 'KB': _KB, 'kb': _KB,
+    'mB': _MB, 'MB': _MB, 'mb': _MB,
+    'gB': _GB, 'GB': _GB, 'gb': _GB
+}
 
 def _VmB(VmKey):
     '''Private.
@@ -28,19 +36,22 @@ def _VmB(VmKey):
     return float(v[1]) * _scale[v[2]]
 
 
-def memory(since=0.0):
+def memory(since=0.0, scale='B'):
     '''Return memory usage in bytes.
     '''
-    return _VmB('VmSize:') - since
+    global _scale
+    return (_VmB('VmSize:') - since) / _scale[scale]
 
 
-def resident(since=0.0):
+def resident(since=0.0, scale='B'):
     '''Return resident memory usage in bytes.
     '''
-    return _VmB('VmRSS:') - since
+    global _scale
+    return (_VmB('VmRSS:') - since) / _scale[scale]
 
 
-def stacksize(since=0.0):
+def stacksize(since=0.0, scale='B'):
     '''Return stack size in bytes.
     '''
-    return _VmB('VmStk:') - since
+    global _scale
+    return (_VmB('VmStk:') - since) / _scale[scale]
