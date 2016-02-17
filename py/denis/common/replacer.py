572885033b6ef8b6f4c581627523bb9deadb6aa2
Denis Griffis <denisrgriffis@gmail.com>
2016-02-17 09:21:53 -0500

    Py3 fixes to replacer; added remove method; added multiOccur bool param to pattern prep

--- py/denis/common/replacer.py
+++ py/denis/common/replacer.py
@@ -5,17 +5,20 @@ class replacer:
     Thanks to: http://stackoverflow.com/questions/6116978/python-replace-multiple-strings
     """
     @staticmethod
-    def prepare(repls, onlyAtEnds=False):
+    def prepare(repls, onlyAtEnds=False, multiOccur=True):
         '''Compiles and returns a regex matching the input list of strings to replace
         Note: returns two values wrapped as one; can feed tuple directly into apply
         '''
         rep = {key: '' for key in repls}
-        rep = dict((re.escape(k), v) for k, v in rep.iteritems())
+        rep = dict((re.escape(k), v) for k, v in rep.items())
         if onlyAtEnds:
-            expr = str.format("{0}|{1}",
-                "|".join(['^%s' % key for key in rep.keys()]),
-                "|".join(['%s$' % key for key in rep.keys()])
-            )
+            if multiOccur:
+                expr = str.format("^[{0}]+|[{0}]+$", ''.join(rep.keys()))
+            else:
+                expr = str.format("{0}|{1}",
+                    "|".join(['^%s' % key for key in rep.keys()]),
+                    "|".join(['%s$' % key for key in rep.keys()])
+                )
         else:
             expr = "|".join(rep.keys())
         pattern = re.compile(expr)
@@ -26,4 +29,9 @@ class replacer:
         '''Uses a compiled pattern from .prepare() to replace all instances of desired strings in text
         '''
         pattern, rep = pattern_rep
-        return pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+        return pattern.sub(lambda m: rep[re.escape(m.group(0))], str(text))
+
+    @staticmethod
+    def remove(pattern_rep, text):
+        pattern, _ = pattern_rep
+        return pattern.sub('', str(text))
