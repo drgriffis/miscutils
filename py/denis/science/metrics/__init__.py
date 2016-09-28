22b313f6a9a437ed558944288cd82bfdaf3c36eb
Denis Griffis <denisrgriffis@gmail.com>
2016-09-28 16:00:38 -0400

    Bugfix for loading local metrics

--- py/denis/science/metrics/__init__.py
+++ py/denis/science/metrics/__init__.py
@@ -4,5 +4,5 @@ Collection of scientific metrics.
 
 __all__ = ['SpearmansRho', 'CosineSimilarity', 'CosineDistance']
 
-from spearmans import SpearmansRho
-from cosine import CosineSimilarity, CosineDistance
+from .spearmans import SpearmansRho
+from .cosine import CosineSimilarity, CosineDistance
