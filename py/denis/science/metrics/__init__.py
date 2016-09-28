c8ff56ab05b2ccf1760f58c7809720e40dba858c
Denis Griffis <denisrgriffis@gmail.com>
2016-09-28 15:56:59 -0400

    Added Spearman's Rho and Cosine similarity/distance metrics for reuse

---
+++ py/denis/science/metrics/__init__.py
@@ -0,0 +1,8 @@
+'''
+Collection of scientific metrics.
+'''
+
+__all__ = ['SpearmansRho', 'CosineSimilarity', 'CosineDistance']
+
+from spearmans import SpearmansRho
+from cosine import CosineSimilarity, CosineDistance
