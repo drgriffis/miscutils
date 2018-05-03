8aa06f9283f2ab001a263489e8dcab31ffcb2612
Denis Newman-Griffis <denisrgriffis@gmail.com>
2018-05-03 10:47:16 -0400

    Added digitsToZero and date/time normalization

--- py/drgriffis/common/preprocessing.py
+++ py/drgriffis/common/preprocessing.py
@@ -42,6 +42,22 @@ _email_normalizer = (
     r'^(mailto:)?([a-zA-Z0-9\.\-_]+\@[a-zA-Z0-9\.\-]+)(\.net|\.com|\.org|\.gov|\.edu|\.co\.uk)', '[EMAIL]'
 )
 
+_digit_plus_s_normalizer = (
+    r'^[0-9]{1,}s$', '[DIGITS]'
+)
+_percent_normalizer = (
+    r'^[0-9]{1,}%$', '[PERCENT]'
+)
+_date_normalizer = (
+    r'((MON|TUE|WED|THU|FRI|monday|tuesday|wednesday|thursday|friday)\s{1,}){0,1}'
+     '([0-9]{4}[-\/][0-9]{1,2}[-\/][0-9]{1,2}|[0-9]{1,2}[-\/][0-9]{1,2}[-\/]([0-9]{4}|[0-9]{2}))'
+     '(\s{1,}[0-9]{1,2}:[0-9]{2}(\s{1,}(AM|PM)){0,1}){0,1}', '[DATETIME]'
+)
+
+_digits_to_zero = (
+    r'[0-9]', '0'
+)
+
 def tokenize(line, clean=True, tolower=True, splitwords=False):
     tokens = line.strip().split()
     if clean:
@@ -61,14 +77,16 @@ def tokenize(line, clean=True, tolower=True, splitwords=False):
         tokens = cleanTokens
     return tokens
 
-def normalizeNumeric(text, generic=True, money=True, phone=True):
+def normalizeNumeric(text, generic=True, money=True, phone=True, digit_plus_s=True, percent=True):
     '''Takes as input a string or a sequence of tokens and returns the
     same string or token sequence.
 
-    Three types of replacement are supported, all enabled by default:
-        money   :: US monetary figures (e.g., $123.45) replaced by [MONEY]
-        phone   :: US phone numbers (e.g., 800-867-5309) replaced by [PHONE]
-        generic :: all other number sequences replaced by [DIGITS]
+    The following types of replacement are supported, all enabled by default:
+        money        :: US monetary figures (e.g., $123.45) replaced by [MONEY]
+        phone        :: US phone numbers (e.g., 800-867-5309) replaced by [PHONE]
+        digit_plus_s :: digits followed by s (e.g., 80s, 1950s) replaced by [DIGITS]
+        percent      :: percentages (e.g., 50%) replaced by [PERCENT]
+        generic      :: all other number sequences replaced by [DIGITS]
 
     The regexes aren't perfect, so take the output with a grain of salt.
     '''
@@ -80,14 +98,24 @@ def normalizeNumeric(text, generic=True, money=True, phone=True):
         for t in tokens:
             # get the list of normalizations, based on specified arguments
             normalizers = []
-            if generic: normalizers.append(_generic_digit_normalizer)
             if money: normalizers.append(_money_normalizer)
+            if digit_plus_s: normalizers.append(_digit_plus_s_normalizer)
+            if percent: normalizers.append(_percent_normalizer)
+            if generic: normalizers.append(_generic_digit_normalizer)
 
             for (ptrn, sub) in normalizers:
                 t = re.sub(ptrn, sub, t)
             normalized.append(t)
         return normalized
     return _normalizeWrapper(text, _normalizer, generic, money)
+
+def digitsToZero(text):
+    def _normalizer(tokens):
+        normalized = []
+        for t in tokens:
+            normalized.append(re.sub(_digits_to_zero[0], _digits_to_zero[1], t))
+        return normalized
+    return _normalizeWrapper(text, _normalizer)
 
 def normalizeURLs(text):
     '''Takes as input a string or a sequence of tokens and returns the
@@ -146,6 +174,21 @@ def normalizePhone(text, extensions=True):
         text = _normalizeWrapper(text, _normalizer, extensions, already_did_phone)
 
     return text
+
+def normalizeDateTime(text):
+    '''Takes as input a string or sequence of tokens and returns the
+    same string or token sequence, with all dates and times replaced
+    by [DATETIME].
+
+    As always, the regexes aren't perfect.  Handle with skepticism.
+    '''
+    def _normalizer(tokens):
+        normalized = []
+        for t in tokens:
+            normalized.append(re.sub(_date_normalizer[0], _date_normalizer[1], t))
+        return normalized
+    return _normalizeWrapper(text, _normalizer)
+        
 
 def _normalizeWrapper(text, method, *args, **kwargs):
     if type(text) is str:
